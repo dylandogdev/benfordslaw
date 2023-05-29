03a55ebbbf9d8b9369d94390838121c7f2dd5c79
Dylan Glenn <dylan@Dylans-MacBook-Air.local>
2023-05-29 00:44:44 -0300

    fixed unit test

--- app/test_util.py
+++ app/test_util.py
@@ -3,6 +3,6 @@ import utils
 
 class TestGetOccurencesByFirstDigit(unittest.TestCase):
     def test_get_occurences_by_first_digit(self):
-        actual = utils.get_occurences_by_first_digit(values=[1,1,2,3,4,0])
-        expected = [2,1,1]
+        actual = utils.get_occurences_by_first_digit(values=["1","1","2","3","4","0"])
+        expected = [2,1,1,1,0,0,0,0,0]
         self.assertEqual(actual, expected)
